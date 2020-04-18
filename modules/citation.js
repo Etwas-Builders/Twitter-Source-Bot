b0044cdfa161d2b45e6cab014210a552bee0bbac
Rahul Tarak <rahultarak12345@gmail.com>
2020-04-18 07:28:38 +0530

    Test Routes and BoilerPlate

---
+++ modules/citation.js
@@ -0,0 +1,16 @@
+var exports = (module.exports = {});
+
+exports.googleNews = async function (data) {
+  // Get Google News Citation
+  let text = data.text;
+
+  return "output"; // Returned output
+};
+
+exports.wiki = async function (data) {
+  // Get Wiki Citation
+
+  let text = data.text;
+
+  return "output"; // Returned output
+};
