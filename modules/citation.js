bafc4e46312bcc9819b347ff0118dfe87c00ae93
rithvikmahin <rithvik7@illinois.edu>
2020-04-19 06:40:15 +0530

    Wikipedia Scraping

--- modules/citation.js
+++ modules/citation.js
@@ -1,27 +1,52 @@
+const axios = require("axios");
+const himalaya = require("himalaya");
+const dom = require("./dom");
+const Fuse = require('fuse.js')
 var exports = (module.exports = {});
 
 exports.googleNews = async function (data) {
   // Get Google News Citation
-  let text = data.text;
-  var url =
-    "http://newsapi.org/v2/everything?" +
-    "q=Apple&" +
-    "from=2020-04-18&" +
-    "sortBy=popularity&" +
-    "apiKey=9f378bb498a54e4f93e2c5dabc13a3e2";
+  let getNewsByKeyword = async (keyword) => {
+    var url =
+      "http://newsapi.org/v2/everything?" +
+      `q=${keyword}&` +
+      "from=2020-04-18&" +
+      "sortBy=popularity&" +
+      "apiKey=9f378bb498a54e4f93e2c5dabc13a3e2";
 
-  var req = new Request(url);
+    var res = await axios.get(url);
+    return res.data;
+  };
 
-  fetch(req).then(function (response) {
-    console.log(response.json());
-  });
+  (async () => {
+    let output = await getNewsByKeyword("Apple");
+    global.output = output;
+    console.log(output.articles);
+  })();
+};
+
+let FindWiki = async (title) => {
+  var url = `https://en.wikipedia.org/api/rest_v1/page/html/${title}`;
+  var output = await axios.get(url);
+  return output.data;
 };
 
 exports.wiki = async function (data) {
   // Get Wiki Citation
-  let response = await fetch();
-  let text = data.text;
+  let title = data.title;
+  let html = await FindWiki(title);
+  let json = await himalaya.parse(html);
+  let document = json.find((el) => tagName == "html");
+  let body = await dom.getElementByTag(document, "body")[0];
+  let texts = await dom.getElementByType(document, "text");
+  let allTextContent = [];
 
-  // Rithvik Test Commit and Author Test
+  for (let text of texts) {
+    if (text.content.includes("\n") && text.content.length <= 2) || text.content == null || text.content.length <=2 {
+      continue;
+    }
+    allTextContent.push(text.content);
+  }
+
   return "output"; // Returned output
 };
