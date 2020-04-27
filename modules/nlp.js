ec204a6a40f2ee4686056d3d6cca437e81ccddbc
Rithvik <rithvik7@illinois.edu>
2020-04-27 01:05:48 -0400

    Created NLP functions to extract words to search, have to integrate into main file

---
+++ modules/nlp.js
@@ -0,0 +1,56 @@
+stopwords = require("stopword");
+wordnet = require("node-wordnet");
+natural = require("natural");
+
+const language = "EN"
+const defaultCategory = 'N';
+const defaultCategoryCapitalized = 'NNP';
+
+var lexicon = new natural.Lexicon(language, defaultCategory, defaultCategoryCapitalized);
+var ruleSet = new natural.RuleSet('EN');
+var tagger = new natural.BrillPOSTagger(lexicon, ruleSet);
+
+function FindWordsToSearch(text) {
+
+  const textArray = text.split(' ');
+  var wordsToSearch = stopwords.removeStopwords(textArray);
+  var finalWords = [];
+  length = wordsToSearch.length
+
+  for (index = 0; index < length; index++) {
+    let currentWord = wordsToSearch[index];
+
+    if (currentWord[0] == "#") {
+      editedWord = currentWord.slice(1, currentWord.length);
+      currentWord = editedWord;
+    }
+
+    let partOfSpeech = GetPartOfSpeech(currentWord);
+
+    if (partOfSpeech == "NNPS" || partOfSpeech == "NNP") {
+      finalWords.push(currentWord);
+      wordsToSearch.splice(index, 1);
+      length = wordsToSearch.length;
+    } else if (partOfSpeech == "NNS" || partOfSpeech == "NN" || partOfSpeech == "N") {
+      finalWords.push(currentWord);
+      wordsToSearch.splice(index, 1);
+      length = wordsToSearch.length;
+    }
+  }
+
+  for (index = 0; index < wordsToSearch.length; index++) {
+      let currentWord = wordsToSearch[index];
+      finalWords.push(currentWord);
+  }
+  return finalWords;
+}
+
+function GetPartOfSpeech(text) {
+  let sentence = tagger.tag(text.split(' '));
+  return sentence['taggedWords'][0]['tag'];
+}
+
+exports.WordsToSearch = async function(text) {
+  let output = await WordsToSearch(text);
+  console.log(output);
+}
