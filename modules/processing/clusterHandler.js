8a7f887ef600634a5c31b9baf33e715063e28cb2
Rahul Tarak <rahultarak12345@gmail.com>
2020-05-15 04:09:54 +0530

    Major Puppeteer Update
    - Added Stealth Mode
    - Recaptcha Support
    - Change Title Retrival Process
    
    Added data model of access denied page
    
    Co-authored-by: Baalateja Kataru <kavesbteja@gmail.com>

--- modules/processing/clusterHandler.js
+++ modules/processing/clusterHandler.js
@@ -1,11 +1,11 @@
 const scrapper = require("./scrapper");
 let main = async function () {
   let urls = [
-    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then",
+    // "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then",
     //"https://www.nytimes.com/2020/05/05/world/coronavirus-news.html",
     //"/home/rahultarak12345/Testing-Repo/modules/processing/image.html",
-    "https://www.vox.com/the-highlight/2020/2/18/21136863/alcoholism-sober-curious-mindful-drinking",
-    "https://twittersourcebot.tech/testing/image.html",
+    "https://www.miamiherald.com/news/coronavirus/article242538781.html",
+    // "https://www.vox.com/the-highlight/2020/2/18/21136863/alcoholism-sober-curious-mindful-drinking",
   ];
   let urlsLength = urls.length;
   let resolvedPromiseCount = 0;
--- modules/processing/scrapper.js
+++ modules/processing/scrapper.js
@@ -1,9 +1,19 @@
 const { Cluster } = require("puppeteer-cluster");
+const vanillaPuppeteer = require("puppeteer");
+
+const { addExtra } = require("puppeteer-extra");
+const Stealth = require("puppeteer-extra-plugin-stealth");
+const Recaptcha = require("puppeteer-extra-plugin-recaptcha");
 
 var exports = (module.exports = {});
 
+const puppeteer = addExtra(vanillaPuppeteer);
+puppeteer.use(Stealth());
+puppeteer.use(Recaptcha());
+
 exports.createCluster = async function () {
   const cluster = await Cluster.launch({
+    puppeteer,
     concurrency: Cluster.CONCURRENCY_CONTEXT,
     maxConcurrency: 20,
   });
@@ -12,20 +22,29 @@ exports.createCluster = async function () {
 
   await cluster.task(async ({ page, data: url }) => {
     console.log("Added url to queue", url);
-    await page.goto(url, { waitUntil: "domcontentloaded" });
 
+    await page.goto(url, { waitUntil: "domcontentloaded" });
+    await page.addScriptTag({
+      url: "https://code.jquery.com/jquery-3.2.1.min.js",
+    });
     await page.waitFor(200);
     const data = await page.evaluate(() => {
       const body = document.querySelector("body");
-      let title = document.title;
+      let title =
+        document.querySelectorAll("head > meta[name='twitter:title']")[0]
+          .content ||
+        document.querySelectorAll("head > meta[name='og:title']")[0].content ||
+        document.querySelectorAll("head > meta[name='title']")[0].content ||
+        document.title;
+
       let text = body.innerText;
-      console.log("Cluster -> Task -> title", title);
+      //console.log("Cluster -> Task -> title", title);
       let data = {};
       data.title = title;
       data.text = text;
       return data;
     });
-    console.log("Data from scrapper", data.title);
+    console.log("Data from scrapper", data.title, data.text.length);
     if (!data.title) {
       data.title = "test";
       //await page.setViewport({ width: 1920, height: 1080 });
