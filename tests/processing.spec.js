28d0392e10becce7a53e0b386b7977ff2c84fba8
Rahul Tarak <rahultarak12345@gmail.com>
2020-05-16 06:54:51 +0530

    Added Access Denied Unit Testing

--- tests/processing.spec.js
+++ tests/processing.spec.js
@@ -12,6 +12,7 @@ describe("Get Top Result", () => {
   it("Get Top Result is a function", () => {
     expect(typeof processing.getTopResult).toBe("function");
   });
+
   it("Get Top Result is active", async () => {
     const results = [{ url: "https://twittersourcebot.tech/" }];
     const username = "whosaidthisbot";
@@ -20,7 +21,7 @@ describe("Get Top Result", () => {
       { word: "tweet", partOfSpeech: "NNS" },
       { word: "Source", partOfSpeech: "NNS" },
     ];
-    const tweetId = "jestTestTweet";
+    const tweetId = "jestPuppeteerActiveTest";
 
     const topResult = await processing.getTopResult(
       results,
@@ -34,6 +35,37 @@ describe("Get Top Result", () => {
     expect(cluster).toBeDefined();
     expect(topResult.topResult).toBeNull;
   });
+
+  it("No Access Denied Error", async () => {
+    const results = [
+      {
+        url:
+          "https://www.miamiherald.com/news/coronavirus/article242538781.html",
+        url:
+          "https://www.seattletimes.com/business/j-c-penney-talks-with-kkr-ares-lenders-on-bankruptcy-loan/",
+      },
+    ];
+    const username = "whosaidthisbot";
+    const keywords = [
+      { word: "Bot", partOfSpeech: "NNS" },
+      { word: "tweet", partOfSpeech: "NNS" },
+      { word: "Source", partOfSpeech: "NNS" },
+    ];
+    const tweetId = "jestNoAccessDeniedTest";
+
+    const topResult = await processing.getTopResult(
+      results,
+      username,
+      keywords,
+      tweetId
+    );
+    expect(topResult).toBeDefined();
+    cluster = topResult.cluster;
+    console.log(topResult);
+    expect(cluster).toBeDefined();
+    expect(topResult.topResult).toBeNull;
+  });
+
   afterAll(async () => {
     let scraper = require("../modules/processing/scraper");
     await scraper.closeCluster(cluster);
