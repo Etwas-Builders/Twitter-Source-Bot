6c67c19c3b2850ec819de0347763fd43862d4181
Rahul Tarak <rahultarak12345@gmail.com>
2020-05-18 01:01:46 +0530

    Handled Jest Open Promises

--- tests/database.spec.js
+++ tests/database.spec.js
@@ -8,14 +8,19 @@ const mongoose = require("mongoose");
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useUnifiedTopology", true);
 
-describe("Check Cached Tweets", () => {
+describe("All Database Test", () => {
   beforeAll(async () => {
-    await mongoose.connect(`mongodb://${process.env.MONGO_URL}`, (err) => {
-      if (err) {
-        console.log(err);
-        expect(err).toThrowError(err);
-      }
-    });
+    try {
+      await mongoose.connect(`mongodb://${process.env.MONGO_URL}`, (err) => {
+        if (err) {
+          console.info(err);
+          expect(err).toThrowError(err);
+        }
+      });
+    } catch (err) {
+      console.info(err);
+      expect(err).toThrowError(err);
+    }
   });
 
   it("Check Tweet Cache is Defined", () => {
@@ -26,8 +31,6 @@ describe("Check Cached Tweets", () => {
   });
   it("Check Tweet Cache with valid tweetId but expired", async () => {
     try {
-      let con = await mongoose.connect(`mongodb://${process.env.MONGO_URL}`);
-
       const tweetId = "1260243604125052929";
       const result = await database.checkTweetCache(tweetId);
       console.log("DB Result", result);
@@ -38,9 +41,26 @@ describe("Check Cached Tweets", () => {
     }
   });
 
+  it("Is Tweet Handled is Defined", () => {
+    expect(database.isTweetHandled).toBeDefined;
+  });
+  it("Is Tweet Handled is a function", () => {
+    expect(typeof database.isTweetHandled).toBe("function");
+  });
+  it("Is Tweet Handled Test", async () => {
+    expect(await database.isTweetHandled("1261877608490446848")).toBeTruthy;
+  });
+  it("Is Tweet Unhandled Test", async () => {
+    expect(await database.isTweetHandled("unhandledJestTest")).toBeFalsy;
+  });
+
   afterAll(async () => {
-    console.log("Database closing");
-    await mongoose.connection.close();
-    console.log("Database closed");
+    try {
+      //console.log("Database closing");
+      await mongoose.connection.close();
+      //console.log("Database closed");
+    } catch (err) {
+      expect(err).toThrowError(`Unable to Close Db \n ${err}`);
+    }
   });
 });
